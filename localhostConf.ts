099b7817c5a9f3e8b75f126ddc0d39658fad00a1
ante31 <92745972+ante31@users.noreply.github.com>
2025-06-21 14:37:48 +0200

    Delete localhostConf.ts

--- localhostConf.ts
+++
@@ -1,5 +0,0 @@
-//export const backendUrl = "https://server-production-579c.up.railway.app";
-export const backendUrl = "http://localhost:3000";
-
-//export const backendUrlBackup = "https://server-three-pink-44.vercel.app";
-export const backendUrlBackup = "http://localhost:3000";
